daa7fdb367785f7a2318b005ce5d210c9688b409
SamRol <1628526495@qq.com>
2025-05-26 21:11:38 +0800

    init

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      
+    </div>
+  );
 }
